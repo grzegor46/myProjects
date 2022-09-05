17b40e2bca86581d7083366bef32a23f7715e8f2
Grzegor46 <grzegor46@gmail.com>
2022-09-05 21:08:36 +0200

    refactor

--- src/main/java/game/Action.java
+++ src/main/java/game/Action.java
@@ -12,7 +12,6 @@ public class Action {
         System.out.println("You used " + item);
     }
 
-
     public void takeItem(Item item) {
 
         backpack.add(item);
@@ -33,7 +32,6 @@ public class Action {
         return null;
     }
 
-
     public void describeItem(String nameItem) {
 
         try {
@@ -42,23 +40,13 @@ public class Action {
         } catch (Exception e) {
             System.out.println("You wanted to check '" + nameItem + "' but item doesn't exist");
         }
-
     }
 
     public void describeRoom () {
         System.out.println("You see a palm, a chest, bricks and plants ");
 
-//        trzeba obejrzec stol, żeby znalezc latarke, które potem mozna wykorzystać podświetlając mapę i znależć skarb.
-
-//        w cegle będzie trzeba użyć młotka, ponieważ w środku będzie kod do skrzyni
-//        mapa jest niekompletna, brakuje kawalka jej, aby byla w calosci.
-//        dopiero po odczytaniu w kokosie mapy jest napis aby rozlupac cegly.
-//        palme bedzie trzeba  2-3 razy poruszyc aby spadl kokos, w ktorym jest  brakujaca czesc mapy
-//        skrzynia ma klodke w ktorej trzeba bedzie wpisac kod (kod bedzie generowany losowo), w skrzyni jest mapa
-//
-//        po zdobyciu mapy można iść odszukać skarb - > koniec gry
-
     }
+
     public void callHelp() {
         System.out.println("if you want explore some place you should type 'check [nameOfPlace]");
         System.out.println("if you write 'touch' you will have some interaction with some subject");
