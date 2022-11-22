ed56b0458bdb90b1092149b4ccf476d67ee3c9ff
Grzegor46 <grzegor46@gmail.com>
2022-11-22 23:39:11 +0100

    implementing nextMove method

--- src/main/java/checkers/game/Game.java
+++ src/main/java/checkers/game/Game.java
@@ -24,10 +24,28 @@ public class Game {
 
         boolean moved = false;
 
-        System.out.println("Please type position from which square would you like move");
+        System.out.println("Please type position from which square would you like to move");
         System.out.println("enter 2 digits, example first is '1'  on 'x' axis and next enter '2' is on 'y' axis");
+
         int[] arrayOfPositionMoveFrom = {scanner.nextInt(), scanner.nextInt()};
         int[] arrayOfPositionMoveTo = {scanner.nextInt(), scanner.nextInt()};
+
+    }
+
+    public boolean validMove(int[] arrayOfPositionMoveFrom, int[] arrayOfPositionMoveTo) {
+
+        int fromX = arrayOfPositionMoveFrom[0];
+        int fromY = arrayOfPositionMoveFrom[1];
+        int toX = arrayOfPositionMoveTo[0];
+        int toY = arrayOfPositionMoveTo[1];
+
+        if((fromX < 0 && fromY < 0) || (fromX > 7 && fromY > 7) || (toX < 0 && toY < 0) || (toX > 7 && toY > 7)){
+            return false;
+        } else if() {
+
+        }
+
+        return false;
 
     }
 }
