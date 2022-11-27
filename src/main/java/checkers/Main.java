20eba47fad548a92b5857032068dd0401f3bd96f
Grzegor46 <grzegor46@gmail.com>
2022-11-27 22:56:42 +0100

    something is working in moving

--- src/main/java/checkers/Main.java
+++ src/main/java/checkers/Main.java
@@ -1,22 +1,24 @@
 package checkers;
 
-import checkers.board.Board;
 import checkers.game.Game;
 
-import java.util.ArrayList;
 
 public class Main {
 
     public static void main(String[] args) {
 //        Referee referee = new Referee();
         Game game = new Game();
-//
-//        game.printboard(board.tablicaPionkow);
 
-        board.initialBoardWithRedAndBlackChecker();
-        board.printBoard();
-        game.getNextMove();
-        board.printBoard();
+        // Setup and print out checker board.
+        game.initialBoardWithRedAndBlackChecker();
+	game.printBoard();
+
+	// Loop until game is over.
+	while (!game.gameOver()) {
+	    //Execute a move and print the board out afterwards.
+	    game.getNextMove();
+	    game.printBoard();
+	}
 
     }
 
--- src/main/java/checkers/game/Game.java
+++ src/main/java/checkers/game/Game.java
@@ -7,23 +7,68 @@ import java.util.Scanner;
 
 public class Game {
 
-    Board board = new Board();
+    //    Board board = new Board();
     WhoesTurn whoesTurn;
-
+    public String[][] field = new String[8][8];
+    int checker_b = 12;
+    int checker_w = 12;
 
     enum WhoesTurn {
         WHITE,
         BLACK
     }
 
-    public void getNextMove(){
+    public void initialBoardWithRedAndBlackChecker() {
+
+        for (int i = 0; i < 8; i += 1) {
+            for (int j = 0; j < 8; j += 1) {
+                field[i][j] = "[ ]";
+            }
+            System.out.println();
+        }
+
+        for (int i = 0; i < 8; i += 1) {
+            for (int j = 1; j < 8; j += 2) {
+                if (i == 0 || i == 2) {
+                    field[i][j] = "[w]";
+                }
+                if (i == 6) {
+                    field[i][j] = "[b]";
+                }
+            }
+            for (int j = 0; j < 8; j += 2) {
+                if (i == 1) {
+                    field[i][j] = "[w]";
+                }
+                if (i == 7 || i == 5) {
+                    field[i][j] = "[b]";
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public void printBoard() {
+        System.out.println("   1   2   3   4   5   6   7   8  X");
+        for (int i = 0; i < 8; i += 1) {
+            System.out.print((i + 1) + " ");
+            for (int j = 0; j < 8; j += 1) {
+                System.out.print(field[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.print("Y");
+    }
+
+
+    public void getNextMove() {
+        WhoesTurn whoesTurn = WhoesTurn.WHITE;
 
         Scanner scanner = new Scanner(System.in);
 
-        if(whoesTurn == WhoesTurn.WHITE) {
+        if (whoesTurn == WhoesTurn.WHITE) {
             System.out.println("It is your turn, white.");
-        }
-	    else
+        } else
             System.out.println("It is your turn, black.");
 
         boolean moved = false;
@@ -39,7 +84,7 @@ public class Game {
         //code with relevant move
         // if valid move then execute move
 
-        if(validMove(arrayOfPositionMoveFrom, arrayOfPositionMoveTo)) {
+        if (validMove(arrayOfPositionMoveFrom, arrayOfPositionMoveTo)) {
             executeMove(arrayOfPositionMoveFrom, arrayOfPositionMoveTo);
         }
 
@@ -47,27 +92,57 @@ public class Game {
 
     public boolean validMove(int[] arrayOfPositionMoveFrom, int[] arrayOfPositionMoveTo) {
 
-        int fromX = (arrayOfPositionMoveFrom[0])-1;
-        int fromY = (arrayOfPositionMoveFrom[1])-1;
-        int toX = (arrayOfPositionMoveTo[0])-1;
-        int toY = (arrayOfPositionMoveTo[1])-1;
+        int fromX = (arrayOfPositionMoveFrom[0]) - 1;
+        int fromY = (arrayOfPositionMoveFrom[1]) - 1;
+        int toX = (arrayOfPositionMoveTo[0]) - 1;
+        int toY = (arrayOfPositionMoveTo[1]) - 1;
 
-        if((fromX < 0 && fromY < 0) || (fromX > 7 && fromY > 7) || (toX < 0 && toY < 0) || (toX > 7 && toY > 7)){
+        if ((fromX < 0 && fromY < 0) || (fromX > 7 && fromY > 7) || (toX < 0 && toY < 0) || (toX > 7 && toY > 7)) {
             return false;
-        } else return (Math.abs(fromX - toX) == 1 || Math.abs(fromY - toY) == 1) && Objects.equals(board.field[toX][toY], "[ ]");
+        } else if ((Math.abs(fromX - toX) == 1) && Objects.equals(field[toY][toX], "[ ]")) {
+            return true;
+        } else if (((Math.abs(fromX - toX) == 2)  && field[toY][toX].equals("[ ]")) && field[(fromY+toY)/2][(fromX+toX)/2].equals("[w]")) {
+            return true;
+        }else if (((Math.abs(fromX - toX) == 2)  && field[toY][toX].equals("[ ]")) && field[(fromY+toY)/2][(fromX+toX)/2].equals("[b]")) {
+            return true;
+        }else {
+                System.out.println("invalid move");
+            }
+            return false;
+        }
 
+
+        public void executeMove ( int[] arrayOfPositionMoveFrom, int[] arrayOfPositionMoveTo){
+
+            int fromX = (arrayOfPositionMoveFrom[0]) - 1;
+            int fromY = (arrayOfPositionMoveFrom[1]) - 1;
+            int toX = (arrayOfPositionMoveTo[0]) - 1;
+            int toY = (arrayOfPositionMoveTo[1]) - 1;
+
+            if ((Math.abs(fromX - toX) == 1) && Objects.equals(field[toY][toX], "[ ]")) {
+                field[toY][toX] = field[fromY][fromX];
+                field[fromY][fromX] = "[ ]";
+            }
+            if((Math.abs(fromX - toX) == 2) && Math.abs(fromY - toY) ==2 ) {
+                field[(fromY+toY)/2][(fromX+toX)/2] = "[ ]";
+                field[toY][toX] = field[fromY][fromX];
+                field[fromY][fromX] = "[ ]";
+                checker_w--;
+            }
+
+
+        }
+    // Checks to see if game is over based on number of checkers left.
+    public boolean gameOver() {
+        return (checker_w == 0 || checker_b == 0);
     }
 
-    public void executeMove(int[] arrayOfPositionMoveFrom, int[] arrayOfPositionMoveTo) {
-
-        int fromX = (arrayOfPositionMoveFrom[0])-1;
-        int fromY = (arrayOfPositionMoveFrom[1])-1;
-        int toX = (arrayOfPositionMoveTo[0])-1;
-        int toY = (arrayOfPositionMoveTo[1])-1;
-
-        board.field[toX][toY] = board.field[fromX][fromY];
-        board.field[fromX][fromY] = "[ ]";
-
-
+    // Returns color of the winner.
+    public String winnerIs() {
+        if (checker_b == 0)
+            return "red";
+        else
+            return "black";
     }
-}
+    }
+
