56afbcf208e7d61fbcbf4b099b3d69c13ff15197
Grzegor46 <grzegor46@gmail.com>
2022-11-27 15:27:02 +0100

    implementing nextMove method

--- src/main/java/checkers/board/Board.java
+++ src/main/java/checkers/board/Board.java
@@ -1,56 +1,51 @@
 package checkers.board;
 
 public class Board {
-    public char[][] board;
+    public String[][] field;
 
     public Board() {
-        this.board = new char[8][8];
+        this.field = new String[8][8];
     }
 
     public void initialBoardWithRedAndBlackChecker() {
 
-        int i,j;
-        for(i=0;i<8;i++) {
-            for(j=0;j<8;j++){
-                board[i][j] = '_';
+        for( int i = 0; i < 8; i += 1) {
+            for (int j = 0; j < 8; j += 1 ){
+                field[i][j] = "[ ]";
             }
+            System.out.println();
         }
-        for(i=0;i<8;i+=2) {
-            board[i][1]= 'r';
-            board[i][5]= 'b';
-            board[i][7]= 'b';
-        }
-        for(i=1;i<8;i+=2) {
-            board[i][0]= 'r';
-            board[i][2]= 'r';
-            board[i][6]= 'b';
+
+        for( int i = 0; i < 8; i += 1) {
+            for (int j = 1; j < 8; j += 2 ){
+                if(i==0 || i==2 ) {
+                    field[i][j] = "[w]";
+                }
+                if(i==6) {
+                    field[i][j] = "[b]";
+                }
+            }
+            for (int j = 0; j < 8; j += 2 ){
+                if(i==1) {
+                    field[i][j] = "[w]";
+                }
+                if(i==7 || i==5) {
+                    field[i][j] = "[b]";
+                }
+            }
+            System.out.println();
         }
     }
     public void printBoard() {
-        int i,j;
-        System.out.println("  1 2 3 4 5 6 7 8 x");
-        for (i=0;i<8;i++) {
+        System.out.println("   1   2   3   4   5   6   7   8  X");
+        for( int i = 0; i < 8; i += 1) {
             System.out.print((i+1) + " ");
-            for (j=0;j<8;j++) {
-                // j - pobiera zerowy indeks z każdego nastepnego rzedu
-                System.out.print(board[j][i]+ " ");
+            for (int j = 0; j < 8; j += 1 ){
+                System.out.print(field[i][j]+" ");
             }
             System.out.println();
         }
-        System.out.println("y");
-    }
-
-    public void printBoard2() {
-        int i,j;
-        System.out.println("  1 2 3 4 5 6 7 8 x");
-        for (i=0;i<8;i++) {
-            System.out.print((i+1) + " ");
-            for (j=0;j<8;j++) {
-                System.out.print(board[i][j]+ " ");
-            }
-            System.out.println();
-        }
-        System.out.println("y");
+        System.out.print("Y");
     }
 
     }
--- src/main/java/checkers/game/Game.java
+++ src/main/java/checkers/game/Game.java
@@ -1,51 +1,73 @@
 package checkers.game;
 
-import javax.sound.midi.Soundbank;
+import checkers.board.Board;
+
+import java.util.Objects;
 import java.util.Scanner;
 
 public class Game {
 
-    enum WhoesTurn {
-        RED,
-        BLACK
-    }
-
+    Board board = new Board();
     WhoesTurn whoesTurn;
 
 
+    enum WhoesTurn {
+        WHITE,
+        BLACK
+    }
+
     public void getNextMove(){
+
         Scanner scanner = new Scanner(System.in);
 
-        if(whoesTurn == WhoesTurn.RED) {
-            System.out.println("It is your turn, red.");
+        if(whoesTurn == WhoesTurn.WHITE) {
+            System.out.println("It is your turn, white.");
         }
 	    else
             System.out.println("It is your turn, black.");
 
         boolean moved = false;
 
-        System.out.println("Please type position from which square would you like to move");
+        System.out.println("Please type position FROM which square would you like to move");
         System.out.println("enter 2 digits, example first is '1'  on 'x' axis and next enter '2' is on 'y' axis");
 
         int[] arrayOfPositionMoveFrom = {scanner.nextInt(), scanner.nextInt()};
+
+        System.out.println("Please type position TO which square would you like to move");
         int[] arrayOfPositionMoveTo = {scanner.nextInt(), scanner.nextInt()};
+
+        //code with relevant move
+        // if valid move then execute move
+
+        if(validMove(arrayOfPositionMoveFrom, arrayOfPositionMoveTo)) {
+            executeMove(arrayOfPositionMoveFrom, arrayOfPositionMoveTo);
+        }
 
     }
 
     public boolean validMove(int[] arrayOfPositionMoveFrom, int[] arrayOfPositionMoveTo) {
 
-        int fromX = arrayOfPositionMoveFrom[0];
-        int fromY = arrayOfPositionMoveFrom[1];
-        int toX = arrayOfPositionMoveTo[0];
-        int toY = arrayOfPositionMoveTo[1];
+        int fromX = (arrayOfPositionMoveFrom[0])-1;
+        int fromY = (arrayOfPositionMoveFrom[1])-1;
+        int toX = (arrayOfPositionMoveTo[0])-1;
+        int toY = (arrayOfPositionMoveTo[1])-1;
 
         if((fromX < 0 && fromY < 0) || (fromX > 7 && fromY > 7) || (toX < 0 && toY < 0) || (toX > 7 && toY > 7)){
             return false;
-        } else if() {
+        } else return (Math.abs(fromX - toX) == 1 || Math.abs(fromY - toY) == 1) && Objects.equals(board.field[toX][toY], "[ ]");
 
-        }
+    }
 
-        return false;
+    public void executeMove(int[] arrayOfPositionMoveFrom, int[] arrayOfPositionMoveTo) {
+
+        int fromX = (arrayOfPositionMoveFrom[0])-1;
+        int fromY = (arrayOfPositionMoveFrom[1])-1;
+        int toX = (arrayOfPositionMoveTo[0])-1;
+        int toY = (arrayOfPositionMoveTo[1])-1;
+
+        board.field[toX][toY] = board.field[fromX][fromY];
+        board.field[fromX][fromY] = "[ ]";
+
 
     }
 }
